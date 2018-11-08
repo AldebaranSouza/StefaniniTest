d74dc5d648dad94c601b53c9e6667d79afa1e455
Aldebaran Souza <aldebaran.souza@grupocitra.com.br>
2018-11-08 22:24:04 +0000

    Aldebaran

--- src/test/java/com/cielo/TestWebhookApplicationTests.java
+++ src/test/java/com/cielo/TestWebhookApplicationTests.java
@@ -1,4 +1,4 @@
-package com.improving.testwebhook;
+/*package com.cielo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,3 +14,4 @@ public class TestWebhookApplicationTests {
 	}
 
 }
+*/
